6c10b13d4642e81349fb6cdd1849c935d5a49c93
Makarov Evgeny <mr.makarov68@gmail.com>
2017-10-03 20:35:45 +0300

    Завел rest controller

---
+++ src/main/java/project/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package project.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("project.controllers")
+public class SpringConfiguration {
+
+}
---
+++ src/main/java/project/controllers/EntityController.java
@@ -0,0 +1,14 @@
+package project.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EntityController {
+
+    @RequestMapping("/")
+    public String all() {
+        return "nothing yet";
+    }
+
+}
