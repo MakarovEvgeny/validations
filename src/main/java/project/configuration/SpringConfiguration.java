ec1b73181e89c1d36bb8e1315ede4934056a6ab5
Makarov Evgeny <mr.makarov68@gmail.com>
2017-10-03 21:07:44 +0300

    Подключил datasource

--- src/main/java/project/configuration/SpringConfiguration.java
+++ src/main/java/project/configuration/SpringConfiguration.java
@@ -1,10 +1,43 @@
 package project.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan("project.controllers")
+@ComponentScan("project.controller")
+@PropertySource("classpath:database.properties")
 public class SpringConfiguration {
+
+    @Value("${host}")
+    private static String host;
+
+    @Value("${port}")
+    private static String port;
+
+    @Value("${login}")
+    private static String login;
+
+    @Value("${password}")
+    private static String password;
+
+    @Value("${database}")
+    private static String database;
+
+    @Bean
+    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertyPlaceholderConfigurer();
+    }
+
+    @Bean
+    public static DataSource dataSource() {
+        return new DriverManagerDataSource(String.format("jdbc:postgresql://%s/%s/%s", host, port, database), login, password);
+    }
 
 }
