1ed654e64260aa198102ded7d881d2577d7ec0f3
Makarov Evgeny <mr.makarov68@gmail.com>
2017-11-11 17:38:04 +0300

    Подключен MessageSource. Некоторые проверки клиентских данных для Entity

--- src/main/java/project/model/entity/EntityValidator.java
+++ src/main/java/project/model/entity/EntityValidator.java
@@ -1,12 +1,14 @@
-package project.controller;
+package project.model.entity;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
-import project.model.entity.Entity;
 
 @Component
 public class EntityValidator implements Validator {
+
+    private static final String NAME = "Сущность";
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -15,7 +17,8 @@ public class EntityValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-
+        ValidationUtils.rejectIfEmpty(errors, "id", "001", new String[] {NAME});
+        ValidationUtils.rejectIfEmpty(errors, "name", "002", new String[] {NAME});
     }
 
 }
