65b3718781bef12f9308281693df877503524989
Makarov Evgeny <mr.makarov68@gmail.com>
2017-11-16 18:21:35 +0300

    Базовый контроллер для базового окна редактирования доменных моделей

--- src/main/resources/public/app/views/EntityWindowController.js
+++ src/main/resources/public/app/views/EntityWindowController.js
@@ -1,52 +1,14 @@
 Ext.define('app.views.EntityWindowController', {
-    extend: 'Ext.app.ViewController',
+    extend: 'app.views.ModelWindowController',
     alias: 'controller.entity-window-controller',
 
     requires: [
         'app.models.Entity'
     ],
 
-    /** Получим данные с формы. */
-    getEntity: function () {
-        var entity = Ext.create('app.models.Entity');
-        this.getView().down('form').updateRecord(entity);
-        return entity;
-    },
-
-    create: function () {
-        var entity = this.getEntity();
-
-        entity.phantom = true; //у нас проставлен id, надо установить этот признак чтобы не выполнилась операция update.
-        this.save(entity);
-    },
-
-    edit: function () {
-        var entity = this.getEntity();
-
-        entity.phantom = false; // установим признак чтобы выполнилась операция update.
-        this.save(entity);
-    },
-
-    delete: function () {
-        var entity = this.getEntity();
-
-        entity.dropped = true;
-        entity.phantom = false; // Установим признаки для удаления записи.
-        this.save(entity);
-    },
-
-    /** @private */
-    save: function(entity) {
-        var w = this.getView();
-        w.setLoading(true);
-        entity.save({
-            callback: function (record, operation, success) {
-                w.setLoading(false);
-                if (success) {
-                    w.close();
-                }
-            }
-        });
+    /** @override */
+    createEmptyModel: function () {
+        return Ext.create('app.models.Entity');
     }
 
 });
---
+++ src/main/resources/public/app/views/ModelWindowController.js
@@ -0,0 +1,58 @@
+Ext.define('app.views.ModelWindowController', {
+    extend: 'Ext.app.ViewController',
+
+    /** @protected */
+    createEmptyModel: function () {
+        throw 'should be overridden';
+    },
+
+    /**
+     * @private
+     * Получим данные с формы.
+     */
+    getModel: function () {
+        var model = this.createEmptyModel();
+        this.getView().down('form').updateRecord(model);
+        return model;
+    },
+
+    /** @protected */
+    create: function () {
+        var model = this.getModel();
+
+        model.phantom = true; //у нас проставлен id, надо установить этот признак чтобы не выполнилась операция update.
+        this.save(model);
+    },
+
+    /** @protected */
+    edit: function () {
+        var model = this.getModel();
+
+        model.phantom = false; // установим признак чтобы выполнилась операция update.
+        this.save(model);
+    },
+
+    /** @protected */
+    delete: function () {
+        var model = this.getModel();
+
+        model.dropped = true;
+        model.phantom = false; // Установим признаки для удаления записи.
+        this.save(model);
+    },
+
+    /** @private */
+    save: function(model) {
+        var w = this.getView();
+        w.setLoading(true);
+        model.save({
+            callback: function (record, operation, success) {
+                w.setLoading(false);
+                if (success) {
+                    w.close();
+                }
+            }
+        });
+    }
+
+});
