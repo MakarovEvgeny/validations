ed2120d9e6824ac2692e9293b370b7f72b97aaf9
Makarov Evgeny <mr.makarov68@gmail.com>
2017-11-27 18:21:40 +0300

    Validation - редактирование

--- src/main/resources/public/app/controllers/ValidationGridController.js
+++ src/main/resources/public/app/controllers/ValidationGridController.js
@@ -12,7 +12,14 @@ Ext.define('app.controllers.ValidationGridController', {
     },
 
     /** @override */
-    loadModel: function (id, requestConfig) {
+    loadModel: function (id, requestConfig, window) {
+        requestConfig.success = function (record) {
+            window.down('form').loadRecord(record);
+            var entities = window.down('custom-tagfield[name=entityIds]');
+            entities.setValue(record.entities().getRange());
+            var operations = window.down('custom-tagfield[name=operationIds]');
+            operations.setValue(record.operations().getRange());
+        };
         app.models.Validation.load(id, requestConfig);
     }
 
