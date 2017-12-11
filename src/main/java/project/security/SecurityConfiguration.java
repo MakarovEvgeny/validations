81576feddd3a32fd3f85de698555a189eb2ddb9f
Makarov Evgeny <mr.makarov68@gmail.com>
2017-12-11 19:27:17 +0300

    GET запросы + поисковые запросы доступны всем

---
+++ src/main/java/project/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package project.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Запросы которые не изменяют состояние ресурсов (в терминологии REST) должны выполняться минуя механизмы аутентификации и авторизации.
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.GET, "/**")
+                .antMatchers(HttpMethod.POST, "/*/query"); // Поисковые запросы.
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+    }
+
+}
