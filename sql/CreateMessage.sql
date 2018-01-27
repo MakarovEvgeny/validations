8ffd7743ac0d0377dec87b9de1e7517aae937c2c
Makarov Evgeny <mr.makarov68@gmail.com>
2018-01-27 12:44:17 +0300

    Message varchar -> text

--- sql/CreateMessage.sql
+++ sql/CreateMessage.sql
@@ -3,7 +3,7 @@ CREATE TABLE message
 	message_id VARCHAR(4) NOT NULL
 		CONSTRAINT message_pkey
 			PRIMARY KEY,
-	text VARCHAR(500) NOT NULL,
+	text TEXT NOT NULL,
 	version SMALLINT DEFAULT 1 NOT NULL,
 	commentary VARCHAR(500)
 );
