f6f5d4babf53fd7e8ef58a33e38b544f16a31e65
Makarov Evgeny <mr.makarov68@gmail.com>
2018-03-25 11:02:31 +0300

    Fixes #11 Экспорт должен быть доступен только пользователям прошедшим аутентификацию Пофиксил this scope

--- src/main/resources/public/app/ServerResponseExceptionHandler.js
+++ src/main/resources/public/app/ServerResponseExceptionHandler.js
@@ -9,9 +9,10 @@ Ext.define('app.ServerResponseExceptionHandler', {
     constructor: function () {
         this.callParent();
 
+        var me = this;
         Ext.Ajax.on('requestexception', function (conn, response, options) {
             // обработка http статусов...
-            this.handleResponse(response);
+            me.handleResponse(response);
         });
     },
 
