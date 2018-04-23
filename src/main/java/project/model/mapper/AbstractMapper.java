33435d9bdc9eb1d47188b2400b979909eeb8eccb
Makarov Evgeny <mr.makarov68@gmail.com>
2018-04-23 14:09:55 +0300

    build fix, аннотация которой нет в зависимостях

--- src/main/java/project/model/mapper/AbstractMapper.java
+++ src/main/java/project/model/mapper/AbstractMapper.java
@@ -2,13 +2,12 @@ package project.model.mapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.istack.internal.Nullable;
 
 abstract class AbstractMapper implements Mapper {
 
     private ObjectMapper mapper = new ObjectMapper();
 
-    String toJsonString(@Nullable Object object) {
+    String toJsonString(Object object) {
         try {
             return mapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
